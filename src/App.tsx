cf0ffc263c2cf97700d3e42461580a8986a16d12
Mian Dawood Bin Rafay <dawoodbinrafaydbr@gmail.com>
2025-11-15 05:42:50 +0500

    deployed

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/campus-go/">  {/* <-- Set basename to your repo name */}
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
